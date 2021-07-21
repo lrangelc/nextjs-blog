29b276e4d56188f2fe1cc74de318fd35f3d0fe31
Luis Rangel <luisrangelc@gmail.com>
2021-07-21 09:58:02 -0600

    add Global Styles

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,25 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
-    return (<><h1>First Post</h1><h2><Link href="/"><a>Back to home</a></Link></h2></>)
-    // return (<><h1>First Post</h1><h2><a href="/">Back to home</a></h2></>)
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <style jsx>{`
+        h2 {
+          background-color: yellowgreen;
+        }
+      `}</style>
+      <h1>First Post</h1>
+      <h2>
+        <Link href='/'>
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  );
+  // return (<><h1>First Post</h1><h2><a href="/">Back to home</a></h2></>)
 }
